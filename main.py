b562354ca97040e7a69fb7588b84bc9436cdfb6a
Frederik Snedevind <fsn@mir-robots.com>
2020-11-09 12:38:49 +0100

    Got started with the script

---
+++ main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import json
+import datetime
+
+window = Tk()
+window.geometry('400x1000')
+
+def createTaskEntriesFromJson(date):
+    # This function looks up the taskDictionary.json, 
+    # and creates a label in the UI for each entry
+    a = 1
+
+
+def addTaskToDictionary(date, task):
+    # Add a new task to the dictionary
+    a = 1
+
+
+
+currentDate = datetime.datetime.now()
+
+dateLabel = Label(window, text=str(currentDate.strftime('%d/%m - %Y')))
+previousDateButton = Button(window, text='<')
+nextDateButton = Button(window, text='>')
+
+
+dateLabel.pack()
+
+pageButtons = {}
+
+
+
+Label(window, text='Create new task').pack()
+
+window.mainloop()
