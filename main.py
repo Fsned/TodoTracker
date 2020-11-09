fb389d002c43bb2f7c2689684857756975ac7b2e
Frederik Snedevind <Fsnedevind@hotmail.com>
2020-11-09 19:13:00 +0100

    Made a lot of stuff work!

--- main.py
+++ main.py
@@ -3,12 +3,38 @@ import json
 import datetime
 
 window = Tk()
-window.geometry('400x1000')
+window.geometry('350x1000')
+
+currentDate = datetime.datetime.now()
+dateLabel = Label(window, text=str(currentDate.strftime('%d/%m - %Y')), font='arial 15 bold')
+dateLabel.place(x=140, y=10)
+
+taskLabels = {}
+tasks = {}
 
 def createTaskEntriesFromJson(date):
     # This function looks up the taskDictionary.json, 
     # and creates a label in the UI for each entry
-    a = 1
+    global taskLabels
+    global tasks
+
+    for a in taskLabels:
+        taskLabels[a].destroy()
+        tasks[a].destroy()
+
+    with open('./taskDictionary.json', 'r') as taskDictionaryFile:
+        taskDictionary = json.loads(taskDictionaryFile.read())
+        
+    for a in taskDictionary:
+        if (str(a) == str(date.strftime('%d/%m - %Y'))):
+            todaysTasks = taskDictionary[a]
+            for enum, b in enumerate(todaysTasks):
+                taskLabels[b] = Label(window, text='-', font = 'arial 14 bold')
+                taskLabels[b].place(x = 10, y = 150 + (30*int(enum)))
+
+                tasks[b] = Label(window, text=todaysTasks[b]['taskName'], font = 'arial 14')
+                tasks[b].place(x = 110, y = 150 + (30*int(enum)))
+            break
 
 
 def addTaskToDictionary(date, task):
@@ -21,26 +47,35 @@ def removeTaskFromDictionary(date, task):
     a = 1
 
 
-currentDate = datetime.datetime.now()
+def changePage(date, deltaDays):
+    global currentDate
+    currentDate = date + datetime.timedelta(days=deltaDays)
+    createTaskEntriesFromJson(currentDate)
+    dateLabel.configure(text=str(currentDate.strftime('%d/%m - %Y')))
+    createTaskEntriesFromJson(currentDate)
 
-dateLabel = Label(window, text=str(currentDate.strftime('%d/%m - %Y')), font='arial 15 bold')
-dateLabel.place(x=140, y=10)
-previousDateButton = Button(window, text='<')
-nextDateButton = Button(window, text='>')
+
+previousDateButton = Button(window, text='<', command=lambda : changePage(currentDate, -1))
+nextDateButton = Button(window, text='>', command=lambda : changePage(currentDate, 1))
 
 previousDateButton.place(x = 80, y = 10)
 nextDateButton.place(x = 280, y = 10)
 
-
-
+createTaskEntriesFromJson(currentDate)
 
 createTaskLabel = Label(window, text='Create new task', font = 'arial 13')
 createTaskLabel.place(x=10, y=50)
 
-newTaskHeadline = Entry(window, width= 32)
+newTaskHeadline = Entry(window, width= 25, justify=LEFT)
 newTaskDescription = Entry(window, width = 32)
 
-newTaskHeadline.place(x = 10, y = 80)
-newTaskDescription.place( x = 10, y = 105)
+newTaskHeadlineLabel = Label(window, text='Task name', justify=LEFT)
+newTaskHeadlineLabel.place(x=10, y = 80)
+
+newTaskDescriptionLabel = Label(window, text='Task description')
+newTaskDescriptionLabel.place(x = 10, y = 110)
+
+newTaskHeadline.place(x = 100, y = 80)
+newTaskDescription.place( x = 100, y = 110)
 
 window.mainloop()
